1bd7a0946b2e34035952edbd49079a74ae9478fe
Rasmus-Emil Hansen <r_emil@live.dk>
2021-03-01 15:40:24 +0100

    cors and fallback response

--- src/components/utils/ts/functions/OnFormSubmit.ts
+++ src/components/utils/ts/functions/OnFormSubmit.ts
@@ -1,4 +1,8 @@
+import { useDispatch } from 'react-redux';
+
 async function OnSubmit (e: any): Promise<any> {
+  const dispatch = useDispatch();
+  
   e.preventDefault();
   // Check credentials
   try {
