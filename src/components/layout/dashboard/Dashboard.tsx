10eb999488f92d86efa3e17bb3ee2d9fd91eead3
re <r_emil@live.dk>
2021-03-06 02:24:15 +0100

    dashboard index page layout

--- src/components/layout/dashboard/Dashboard.tsx
+++ src/components/layout/dashboard/Dashboard.tsx
@@ -10,24 +10,7 @@ export default function Dashboard() {
     const canvas: any = document.getElementById("test");
     if(canvas) {
       const ctx = canvas.getContext("2d");
-      const myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: ['1/1', '1/3', '1/5', '1/6', '1/7', '1/8'],
-          datasets: [{
-            label: 'Games won',
-            data: [1, 10, 2, 15, 20, 23],
-            borderColor: [
-              'rgba(42, 157, 143, 1)'
-            ],
-            borderWidth: 4
-          }]
-        },
-        options: {
-          responsive: true
-        }
-      });
-    return myChart;
+      renderChart(ctx);
     }
   }
   useEffect(() => {
--- src/components/utils/ts/functions/renderChart.ts
+++ src/components/utils/ts/functions/renderChart.ts
@@ -1,7 +1,24 @@
 import Chart from 'chart.js';
 
-const renderChart = ({ctx}: any): any => {
-  
+const renderChart = (ctx: any): any => {
+  const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: ['1/1', '1/3', '1/5', '1/6', '1/7', '1/8'],
+      datasets: [{
+        label: 'Games won',
+        data: [1, 10, 2, 15, 20, 23],
+        borderColor: [
+          'rgba(42, 157, 143, 1)'
+        ],
+        borderWidth: 4
+      }]
+    },
+    options: {
+      responsive: true
+    }
+  });
+  return myChart;
 }
 
 export default renderChart;
