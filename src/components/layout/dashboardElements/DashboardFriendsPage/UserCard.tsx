76e7a7b0ac80dd0968d0c3f5e646002efec225fc
Rasmus-Emil Hansen <r_emil@live.dk>
2021-03-08 14:18:20 +0100

    temp user page

--- src/components/layout/dashboardElements/DashboardFriendsPage/UserCard.tsx
+++ src/components/layout/dashboardElements/DashboardFriendsPage/UserCard.tsx
@@ -1,7 +1,12 @@
-export default function UserCard({user}: any): JSX.Element {
+export default function UserCard<user extends {name: string; email: string;}>({user}: any): JSX.Element {
   return (
-    <div>
-      { user.name }
+    <div className="user-card displayFlexAndCenter">
+      <span className="name">
+        { user.name }
+      </span>
+      <span className="invite-user">
+        <span className="mdi mdi-account-plus"></span>
+      </span>
     </div>
   )
 }
--- src/components/utils/ts/constants/users.ts
+++ src/components/utils/ts/constants/users.ts
@@ -7,10 +7,10 @@ import { UserInterface } from '../../../../interfaces/interfaces';
   * Routes
 */
 export const users: UserInterface[] = [
-  { name: 'User 1', email: 'test1@test2.dk'},
-  { name: 'User 2', email: 'test1@test2.dk'},
-  { name: 'User 3', email: 'test1@test2.dk'},
-  { name: 'User 4', email: 'test1@test2.dk'},
-  { name: 'User 5', email: 'test1@test2.dk'},
-  { name: 'User 6', email: 'test1@test2.dk'}
+  { name: 'Lasse', email: 'test1@test2.dk'},
+  { name: 'Martin', email: 'test1@test2.dk'},
+  { name: 'Nick', email: 'test1@test2.dk'},
+  { name: 'Daniel', email: 'test1@test2.dk'},
+  { name: 'Jonas', email: 'test1@test2.dk'},
+  { name: 'Louise', email: 'test1@test2.dk'}
 ];
