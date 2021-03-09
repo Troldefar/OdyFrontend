b6f7ebb043ab50fbc97407c469055e043b463cf6
re <r_emil@live.dk>
2021-03-09 23:45:46 +0100

    stats layout test

--- src/components/layout/dashboard/Statistics.tsx
+++ src/components/layout/dashboard/Statistics.tsx
@@ -13,14 +13,104 @@ export default function Statistics(): JSX.Element {
       <div className="dashboard-right">
         <LoggedInMenu />
         <div className="dashboard-right-lower-statistics-section">
-          <div className="upper">
-            upper
+          <div className="item-1 displayFlexAndCenter showStat">
+            <h4>
+              Headshots %
+            </h4>
+            <p>
+              34
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-down-circle crimsonColor"></span>
+            </span>
           </div>
-          <div className="middle">
-            middle
+          <div className="item-2 displayFlexAndCenter showStat">
+            <h4>
+              Bombs planted
+            </h4>
+            <p>
+              200
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-up-circle greenColor"></span>
+            </span>
           </div>
-          <div className="lower">
-            lower
+          <div className="item-3 displayFlexAndCenter showStat">
+            <h4>
+              Enemies flashed (Pr game avg)
+            </h4>
+            <p>
+              10
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-down-circle crimsonColor"></span>
+            </span>
+          </div>
+          <div className="item-4 displayFlexAndCenter showStat">
+            <h4>
+              Utility damage
+            </h4>
+            <p>
+              300 (Pr game avg)
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-up-circle greenColor"></span>
+            </span>
+          </div>
+          <div className="item-5 displayFlexAndCenter showStat">
+            <h4>
+              Games won
+            </h4>
+            <p>
+              12
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-down-circle crimsonColor"></span>
+            </span>
+          </div>
+          <div className="item-6 displayFlexAndCenter showStat">
+            <h4>
+              Deaths
+            </h4>
+            <p>
+              50
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-up-circle greenColor"></span>
+            </span>
+          </div>
+          <div className="item-7 displayFlexAndCenter showStat">
+            <h4>
+              Riffle kills
+            </h4>
+            <p>
+              10 (Pr game avg)
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-down-circle crimsonColor"></span>
+            </span>
+          </div>
+          <div className="item-8 displayFlexAndCenter showStat">
+            <h4>
+              Sniper kills
+            </h4>
+            <p>
+              4 (Pr game avg)
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-down-circle crimsonColor"></span>
+            </span>
+          </div>
+          <div className="item-9 displayFlexAndCenter showStat">
+            <h4>
+              Pistol kills
+            </h4>
+            <p>
+              7 (Pr game avg)
+            </p>
+            <span className="arrow">
+              <span className="mdi mdi-arrow-up-circle greenColor"></span>
+            </span>
           </div>
         </div>
       </div>
