56c5d4c3e84f17a2c642e90bdc74a79408e3f349
re <r_emil@live.dk>
2021-03-11 11:30:58 +0100

    switch

--- src/components/layout/dashboard/Statistics.tsx
+++ src/components/layout/dashboard/Statistics.tsx
@@ -7,15 +7,7 @@ import StatCard from '../dashboardElements/StatisticsPage/StatCard';
 import { gameStatistics } from '../../utils/ts/constants/gameStatistics';
 
 export default function Statistics(): JSX.Element {
-<<<<<<< HEAD
   const [gameState, setGameState] = useState(gameStatistics[0]);
-  console.log("GS: ", gameState);
-=======
-  const [gameState, setGameState] = useState({
-    title: '',
-    data: []
-  });
->>>>>>> 841e3a11e1177e73098a52ab039e477fef308918
   const slideLeft = () => {
     setGameState(gameStatistics[gameState.index - 1]);
   }
@@ -36,22 +28,12 @@ export default function Statistics(): JSX.Element {
           <div className="menu-switch displayFlexAndCenter">
             <span onClick={slideLeft} className="mdi mdi-arrow-left-bold-circle"></span>
             <p>
-<<<<<<< HEAD
               { gameState.title }
-=======
-              {
-                gameStatistics[0].title
-              }
->>>>>>> 841e3a11e1177e73098a52ab039e477fef308918
             </p>
             <span onClick={slideRight} className="mdi mdi-arrow-right-bold-circle"></span>
           </div>
           {
-<<<<<<< HEAD
             gameState.data.map((stat: any) => (
-=======
-            gameStatistics[0].data.map((stat: any) => (
->>>>>>> 841e3a11e1177e73098a52ab039e477fef308918
               <StatCard
                 key={stat.title}
                 title={stat.title} 
